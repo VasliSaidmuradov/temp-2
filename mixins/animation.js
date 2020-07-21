ec7bb786dfa02b30f93a3035ff2c7b7766cd5eb8
Tamerlan <aytuarov01@gmail.com>
2020-07-21 17:39:11 +0600

    anim triggerhook

--- mixins/animation.js
+++ mixins/animation.js
@@ -4,7 +4,8 @@ export default {
             let item = this.$refs.animationBlock.querySelectorAll('.animation-item')
 
             const scene = this.$scrollmagic.scene({
-                triggerElement: this.$refs.animationBlock
+                triggerElement: this.$refs.animationBlock,
+                triggerHook: 0.7
             })
             .setTween(
                 gsap.from(item, {opacity: 0, y: 15, duration: 0.4, delay: 0.3, stagger: 0.1, ease: 'back.out(3)'})
