ecb3b05fee047b23965667a6b59d7212524bc189
Vasli Saidmuradov <vasli.said@gmail.com>
2020-08-03 16:40:06 +0600

    feat: add translate to error page 404

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,8 +4,8 @@
       <!-- page: {{ page }} -->
       <div class="error-page-block" v-if="!page || error.statusCode === 404">
         <img src="/icons/404.svg" alt="CashU image" class="error-page-image" />
-        <h4 class="error-page-title">Извините. Страница, которую вы ищете, не найдена</h4>
-        <nuxt-link class="button" to="/">Вернуться на главную</nuxt-link>
+        <h4 class="error-page-title">{{ langs[currentLang]['body.not_found'] }}</h4>
+        <nuxt-link class="button" to="/">{{ langs[currentLang]['body.back_to_main'] }}</nuxt-link>
       </div>
     </div>
   </div>
