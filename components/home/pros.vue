43e84a28957293d54d3310ad78b4cb34006fe907
Tamerlan <aytuarov01@gmail.com>
2020-08-12 17:04:46 +0600

    fix: sitemap

--- components/home/pros.vue
+++ components/home/pros.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home-pros" ref="animationBlock">
-        <h3 class="section-title animation-item">{{ langs[currentLang]['body.why_profitable'] }}</h3>
+        <h1 class="section-title animation-item">{{ langs[currentLang]['body.why_profitable'] }}</h1>
         <div class="home-pros-row">
             <div class="home-pros-col animation-item" v-for="(item, i) in extras" :key="i">
                 <div class="home-pros-img-wrp">
